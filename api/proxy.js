ab6ef45b0b1d4780aa8e229c162d840e2c8b2d82
hj33200 <haro.julien33200@gmail.com>
2026-06-01 14:40:21 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,17 @@
 export default async function handler(req, res) {
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-apisports-key');
-    res.status(200).end();
-    return;
-  }
-
-  const { endpoint, league, season } = req.query;
+  const { endpoint, league, season, date } = req.query;
   const API_KEY = 'fa96c57b2732lcef9fc4cae58aa3fe13';
   
   try {
-    const url = `https://v3.football.api-sports.io/${endpoint}?league=${league}&season=${season}`;
+    let url = `https://v3.football.api-sports.io/${endpoint}?league=${league}&season=${season}`;
+    if (date) url += `&date=${date}`;
+    
+    console.log('Calling API:', url);
     
     const response = await fetch(url, {
-      headers: { 'x-apisports-key': API_KEY }
+      headers: { 
+        'x-apisports-key': API_KEY
+      }
     });
     
     const data = await response.json();
